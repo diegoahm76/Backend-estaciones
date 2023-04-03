944b6fdd6f179f55dabd4497e8770420d99c663b
Alejandro Sastoque <sastoque42@gmail.com>
2023-04-03 09:46:55 -0500

    Prueba envio 2

--- estaciones/cron.py
+++ estaciones/cron.py
@@ -1,4 +1,9 @@
+import datetime
+
 def test_cronjob():
+    with open("/tmp/cronlog.txt", "a") as f:
+        f.write("test_cronjob() called at {}\n".format(datetime.datetime.now()))
+    
     msg = "FUNCIONA?"
     print(msg)
     return msg
