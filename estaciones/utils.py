657dd89779769d6f034ecbf6003db06b564bacae
Alejandro Sastoque <sastoque42@gmail.com>
2023-04-23 17:15:30 -0500

    Cambios en la transferencia de datos

--- estaciones/utils.py
+++ estaciones/utils.py
@@ -1045,9 +1045,9 @@ def transfer_data_alertas():
         print(f"Ha ocurrido un error: {e}")
 
 
-# schedule.every(5).minutes.do(transfer_data_datos)
-# schedule.every(5).minutes.do(transfer_data_alertas)
-schedule.every(5).minutes.do(enviar_alertas)
+schedule.every(5).minutes.do(transfer_data_datos)
+schedule.every(5).minutes.do(transfer_data_alertas)
+#schedule.every(5).minutes.do(enviar_alertas)
 
 while True:  # Ciclo principal del programa
     schedule.run_pending()  # Ejecutar tareas pendientes en el horario programado
